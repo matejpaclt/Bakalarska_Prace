152661781874edf19fc6d5d007d20608d291ed8a
matejpaclt <92588479+matejpaclt@users.noreply.github.com>
2024-04-19 20:51:23 +0200

    files 5

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Route handler for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
